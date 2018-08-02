3bbf4a9dabfa9d3acd7103c5ae2eed296a285b51
swutter <42045341+swutter@users.noreply.github.com>
2018-08-02 15:48:58 -0400

    Update AddtoGroup.ps1

--- AddtoGroup.ps1
+++ AddtoGroup.ps1
@@ -7,8 +7,8 @@ Add-ADGroupMember -id 'NA01_Dev_WindowsUpdates_G1' -members $computer
 }
 
 
-$SvcUser = 'dev.adcinternal.com\ssna01jdom'
-$SvcInput = '76492d1116743f0423413b16050a5345MgB8AHgAZABZAGEAeAB0AHgATQB6ACsAMABuAGwATwBUAE8AegBWAFQAbQBOAHcAPQA9AHwAYgA5AGYAMQBmADYANQAyAGQAZQA3AGQAOQBhADgANQAyADAAMAA5AGIAMAAwADIAYgBmADQAOQBkAGMAYQBhAGMAZQAwADEANAA0AGUAZQAwAGIAZgBlADYAYQA3ADUANABlADUANwBkAGIAYQAyADAAZABiADgAYQBiADAANAA='
+$SvcUser = ''
+$SvcInput = ''
 $tok = $SvcInput | ConvertTo-SecureString -key (1..16)
 $credential = New-Object System.Management.Automation.PsCredential($SvcUser,$tok)
 $jobs = @()
